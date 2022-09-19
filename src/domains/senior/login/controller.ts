475181c712f5c2c0a70e01c0f0aa58d368af76b1
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-09-18 23:01:02 -0300

    feat: change implementation with result

--- src/domains/senior/login/controller.ts
+++ src/domains/senior/login/controller.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from "express";
 import { api } from "../../../configs/api";
 import { SeniorService } from "./../../../services/senior/senior";
-import { LoginUseCase } from "./use-cases/login-use-case";
+import { LoginUseCase } from "./use-case";
 
 export class SeniorController {
-  static login(req: Request, res: Response) {
+  static async login(req: Request, res: Response) {
     const service = new SeniorService(api);
     const useCase = new LoginUseCase(service);
 
-    const result = useCase.execute({
+    const result = await useCase.execute({
       user: req.body.user,
       password: req.body.password,
     });
 
-    return res.json(result);
+    if (result.isFailure) {
+      return res.status(result.code).json(result.error);
+    }
+    return res.json(result.getValue());
   }
 }
