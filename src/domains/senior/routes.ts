8b838f26f1540209347bec48cadc58c060f9d28f
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-09-18 23:01:19 -0300

    feat: change directory from login routes

--- src/domains/senior/routes.ts
+++ src/domains/senior/routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import { validateFields } from "./../../../utils/fields-validation-middleware";
-import { SeniorController } from "./controller";
-import { loginSchema } from "./schemas/login-schema";
+import { validateFields } from "../../utils/fields-validation-middleware";
+import { SeniorController } from "./login/controller";
+import { loginSchema } from "./login/schema";
 const router = express.Router();
 
 router.post(
