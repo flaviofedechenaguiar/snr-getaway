999427a482327c14605a3b46d7be19148bca715a
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-09-19 23:44:16 -0300

    feat: fix token-mapper

--- src/services/senior/token-mapper.ts
+++ src/services/senior/token-mapper.ts
@@ -1,10 +1,10 @@
 import { CookieJar } from "tough-cookie";
 
-const KEY = "com.senior.pau.token";
+const KEY = "com.senior.token";
 
 export const tokenMapper = function (jar: CookieJar): string {
   const { cookies } = jar.toJSON();
   const { value } = cookies.find((element) => element.key === KEY);
-  const { access_token } = JSON.parse(JSON.parse(value));
+  const { access_token } = JSON.parse(decodeURIComponent(value));
   return access_token;
 };
