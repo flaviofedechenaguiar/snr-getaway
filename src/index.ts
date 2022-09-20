3a21216fabb3ae1de7dd4b4568c7c2299c555a25
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-09-19 23:45:06 -0300

    feat: change to env config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { ENV } from "./configs/env";
 import server from "./server";
 
 server.listen(process.env.PORT, () => {
-  console.log(`Server running http://localhost:${process.env.PORT}/`);
+  console.log(`Server running http://localhost:${ENV.port}/`);
 });
