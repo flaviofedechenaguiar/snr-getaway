a64fe9228e8f393bb2bfe5a1284bf594fd4dc1c5
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-09-20 22:56:43 -0300

    feat: add middleware in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
+import "express-async-errors";
 import { corsConfig } from "./configs/cors";
+import { errorHandlingMiddleware } from "./middleware/error-handling";
 import router from "./routes";
 const app = express();
 
@@ -9,5 +11,6 @@ app.use(express.json());
 app.use(cors(corsConfig));
 
 app.use(router);
+app.use(errorHandlingMiddleware);
 
 export default app;
