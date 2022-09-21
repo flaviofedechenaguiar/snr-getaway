f45971ea9c9a0f96178b2968a97868717df50cc6
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-09-20 22:50:03 -0300

    feat: create error-handling middleware

---
+++ src/middleware/error-handling.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { ApiError } from "../helpers/api-errors";
+
+export const errorHandingMiddleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof ApiError) {
+    return res
+      .status(err.statusCode)
+      .json({ message: err.message, errors: err.errors });
+  }
+
+  return res.status(500).send("An unexpected error has occurred.");
+};
