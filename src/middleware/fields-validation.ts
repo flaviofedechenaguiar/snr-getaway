29cdb699f846d206c265c0f060bdf350c81c7608
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-09-20 23:13:38 -0300

    feat: change to UnprocessableEntity

--- src/middleware/fields-validation.ts
+++ src/middleware/fields-validation.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { ObjectSchema, ValidationOptions } from "joi";
-import { ApiError } from "../helpers/api-errors";
+import { UnprocessableEntity } from "../helpers/api-errors";
 
 type Property = "body" | "query";
 
 const OPTIONS_CONFIG: ValidationOptions = {
+  abortEarly: false,
   errors: {
     wrap: {
       label: "",
@@ -20,6 +21,6 @@ export const validateFieldsMiddleware = (
     const { error } = schema.validate(req[prop], OPTIONS_CONFIG);
     if (!error) return next();
     const response = error.details.map((detail) => detail.message);
-    throw new ApiError("Invalid data", 422, response);
+    throw new UnprocessableEntity(response);
   };
 };
