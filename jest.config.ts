b2ed16fa9e3ec02adc0df3be73b1d3beb59d60af
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-09-27 23:30:40 -0300

    chore: change config from jest

--- jest.config.ts
+++ jest.config.ts
@@ -153,7 +153,7 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/src/**/*.test.ts?(x)"],
+  testMatch: ["**/__tests__/**/*.test.ts?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
