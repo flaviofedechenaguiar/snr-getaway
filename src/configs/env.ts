f3d98020c1e770ab8652c1297835650c32893eff
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2022-10-31 22:12:24 -0300

    feat: add development validation

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,3 +1,7 @@
+import * as dotenv from "dotenv";
+
+if (process.env.NODE_ENV === "development") dotenv.config();
+
 export const ENV = {
   port: process.env.PORT,
   cors_origin: process.env.CORS_ORIGIN,
